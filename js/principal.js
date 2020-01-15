ec63c7fefcba56f117ed747679dc0bcc2e24ea36
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-01-15 17:27:22 -0300

    Curso JavaScript

--- js/principal.js
+++ js/principal.js
@@ -4,15 +4,32 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector("#primeiro-paciente"); //pega a tr toda
 var tdPeso = paciente.querySelector(".info-peso"); //pega a td toda
 var peso =(tdPeso.textContent); 
-console.log(peso);// pega o valor da td peso
+var pesoValido = true;
+var alturaValida = true;
+var tbImc = document.querySelector(".info-imc");
+
+if(peso < 0 || peso > 500){
+    console.log("Peso invalido");
+    pesoValido = false;
+    tbImc.textContent = "Peso inválido";
+}
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-console.log(altura);
 
+if(altura < 0 || altura > 3.00){
+console.log("Altura invalida");
+alturaValida = false;
+tbImc.textContent = "Altura inválida";
+}
+
+if(pesoValido && alturaValida){
 var imc = peso / (altura * altura);
 console.log(imc);
 
+if(imc >= 0 && imc < 18,5) {
+    console.log("Magresa");
+}
 if(imc > 18,5 && imc < 25){
     console.log("Normal");
 }
@@ -26,5 +43,6 @@ if(imc >= 40){
     console.log("Obesidade Morbida");
 }
 
-var tbImc = document.querySelector(".info-imc");
+
 tbImc.textContent = imc;
+}
