f50efc093347fdb74603e8a363752741ba80116e
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-01-16 16:19:46 -0300

    Curso JavaScript Alura

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,6 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+var titulo = document.querySelector(".titulo"); // salva o titulo do html
+titulo.textContent = "Aparecida Nutricionista"; // altera o titulo
+
 
 var pacientes = document.querySelectorAll(".paciente");
 
@@ -19,21 +20,60 @@ for (var i = 0; i < pacientes.length; i++) {
     var alturaEhValida = true;
 
     if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
-        paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
-        paciente.style.color = "red";
+        paciente.classList.add("paciente-invalido");
+        // paciente.style.color("red"); altera o css 
     }
 
-  if (alturaEhValida && pesoEhValido) {
+    if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
 }
+titulo.addEventListener("click", mostraMensagem); // escutador de evento click
+
+function mostraMensagem(){
+    console.log("olá eu fui clicado");
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+   
+    console.log(form.altura.value); // ve o dado escrito no form
+
+var nome = form.nome.value;
+var peso = form.peso.value;
+var altura = form.altura.value;  // Pega os dados do form
+var gordura = form.gordura.value;
+
+var pacienteTr = document.createElement("tr"); // cria tabela
+
+var nomeTd = document.createElement("td"); // cria linha da tabela
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura; // aplica os valores do form na linha
+gorduraTd.textContent = gordura;
+
+pacienteTr.appendChild(nomeTd); // colocando as linhas dentro da tabela
+pacienteTr.appendChild(pesoTd);
+pacienteTr.appendChild(alturaTd);
+pacienteTr.appendChild(gorduraTd);
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.appendChild(pacienteTr); // coloca o tr dentro da tabela html
+
+});
