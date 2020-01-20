af77ef653c2a542b2a90626c159f9eec5293e670
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-01-20 17:28:30 -0300

    Terminando a parte de filtros

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,9 +2,10 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("dblclick", function () {
-    var AlvoEvento = event.target;
-    var paiDoAlvo = AlvoEvento.parentNode;
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
 
-    paiDoAlvo.remove();
+    setTimeout(function () {
+        event.target.parentNode.remove();
+    }, 500);
 });
