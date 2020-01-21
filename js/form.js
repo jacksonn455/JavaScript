a0f1f34d9b34a34aa0933ae174a242e6bb61bfdb
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-01-21 14:42:25 -0300

    termino do curso

--- js/form.js
+++ js/form.js
@@ -14,19 +14,27 @@ botaoAdicionar.addEventListener("click", function (event) {
         return;
     }
 
+    adicionaPacienteNaTabela(paciente);
+
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
 
     form.reset();
-    var mensagensErro =  document.querySelector("#mensagens-erro");
+    var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
 
-function exibeMensagensDeErro(erros){ 
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
+function exibeMensagensDeErro(erros) {
     var ul = document.querySelector("#mensagens-erro");
     ul.innerHTML = "";
 
-    erros.forEach(function(erro){
+    erros.forEach(function (erro) {
         var li = document.createElement("li");
         li.textContent = erro;
         ul.appendChild(li);
@@ -67,31 +75,31 @@ function montaTd(dado, classe) {
     return td;
 }
 
-  function validaPaciente(paciente) {
+function validaPaciente(paciente) {
 
     var erros = [];
 
-    if (paciente.nome.length == 0){
+    if (paciente.nome.length == 0) {
         erros.push("O nome não pode ser em branco");
     }
 
-    if (paciente.gordura.length == 0){
+    if (paciente.gordura.length == 0) {
         erros.push("A gordura não pode ser em branco");
     }
 
-    if (paciente.peso.length == 0){
+    if (paciente.peso.length == 0) {
         erros.push("O peso não pode ser em branco");
     }
 
-    if (paciente.altura.length == 0){
+    if (paciente.altura.length == 0) {
         erros.push("A altura não pode ser em branco");
     }
 
-    if (!validaPeso(paciente.peso)){
+    if (!validaPeso(paciente.peso)) {
         erros.push("Peso é inválido");
     }
 
-    if (!validaAltura(paciente.altura)){
+    if (!validaAltura(paciente.altura)) {
         erros.push("Altura é inválida");
     }
 
